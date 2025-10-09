3019c3d906d0926dcbb4cd59e23a45c7edf12fed
Nguyễn Hoàng Thiện <nguyenhoangthien120304@gmail.com>
2025-10-09 22:46:47 +0700

    Use InMemory DB for Render demo

--- AIHubTaskTracker/Program.cs
+++ AIHubTaskTracker/Program.cs
@@ -4,7 +4,8 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseInMemoryDatabase("TestDb"));
+
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAll", policy =>
