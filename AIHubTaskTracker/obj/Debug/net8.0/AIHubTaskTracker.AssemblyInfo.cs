1b4f9421843e32a72eee6c1e2330dc0a3a22efdc
hoangminhlong123 <160829670+hoangthienisme@users.noreply.github.com>
2025-10-23 09:27:08 +0700

    Clean code

--- AIHubTaskTracker/obj/Debug/net8.0/AIHubTaskTracker.AssemblyInfo.cs
+++ AIHubTaskTracker/obj/Debug/net8.0/AIHubTaskTracker.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AIHubTaskTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bfd027fab1653a16730ee436df1c5c56ddcb6924")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56574ba50ec72a145a61ed8dd3db1fdb61dde156")]
 [assembly: System.Reflection.AssemblyProductAttribute("AIHubTaskTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AIHubTaskTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
